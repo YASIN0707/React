d1eb086ad9899c4f5f205eed86574d597f845997
YASIN0707 <skyasin315@gmail.com>
2024-08-14 15:50:47 +0530

    test complete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Message from './Components/Message'
+import Quatity from './Components/Quatity'
+import Message1 from './Components/Message1'
+import Message2 from './Components/Message2'
+import React,  {Fragment} from 'react'
+let App=()=>{ 
+  return <div>
+    <h1>App comp</h1>
+    <Fragment/>
+    <Message/>
+    <Quatity/>
+    <Message1/>
+    <Message2/>
+    
+  </div>
+}
+export default App
