77f60d24a4951689e4112aca5ee61b1b3847bcd9
YASIN0707 <skyasin315@gmail.com>
2024-08-16 17:16:55 +0530

    tst

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import Message from './Components/Message'
-import Quatity from './Components/Quatity'
-import Message1 from './Components/Message1'
-import Message2 from './Components/Message2'
+import Messagewithoutbinding from './Components/Messagewithoutbinding'
 import React,  {Fragment} from 'react'
 let App=()=>{ 
   return <div>
     <h1>App comp</h1>
-    <Fragment/>
-    <Message/>
-    <Quatity/>
-    <Message1/>
-    <Message2/>
-    
+    <Messagewithoutbinding/>
   </div>
 }
 export default App
