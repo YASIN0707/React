e33b81e6a40e7b38772f84f30cc903089168c630
YASIN0707 <skyasin315@gmail.com>
2024-08-20 18:37:11 +0530

    tst

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CompA from './propsdrilling/CompA'
+import Users2 from './Users2'
 let App=()=>{ 
   
   return <div>
     <h1>Compenent A</h1>
-   <CompA/>
+   <Users2/>
   </div>
 }
 export default App
